f5b1b12a05a3072c62642b659f47bbc9ecb77b61
maxvanbart <maxvanbart@gmail.com>
2023-03-30 15:41:45 +0200

    comments

--- network2.py
+++ network2.py
@@ -2,12 +2,14 @@ import torch
 from tqdm import tqdm
 import json
 import numpy as np
+import pandas as pd
 
 from network import Net, Image
 
 
 def main():
     print("Hello world!")
+    # Initialize network based on trained parameters from the network.py file
     net = Net()
     net.load_state_dict(torch.load("model/yolo1"))
     net.eval()
@@ -15,17 +17,21 @@ def main():
     # Load labelbox json file for hand drawn boxes
     with open('data.json') as file:
         data = json.load(file)
-
+    # Show length of the labelbox data
     print(f"Data length: {len(data)}.")
 
+    # Create an empty list to store the initialized image objects
     images = []
-    # for i in tqdm(range(len(data))):
-    # for j in range(10):
-    for i in tqdm(range(1600, 1700)):
+    # Load the images in the desired range for verification
+    for i in tqdm(range(1000, 1100)):
+        # Extract the indexed json format things
         dat = data[i]
 
+        # Initialize an image class object using the name of the image
         thing = Image(dat["External ID"])
 
+        # For all obstacles stored in the labeled data calculate the parameters which the yolo paper desires
+        # as outputs
         for item in dat["Label"]["objects"]:
             value = item["value"]
             bbox = item["bbox"]
@@ -35,9 +41,11 @@ def main():
             l = bbox["left"]
             bbox["x"] = round(t + h / 2)
             bbox["y"] = round(l + w / 2)
-
+            # Store the data on the bounding box of the object and the object type in a tuple in the list containing
+            # the objects.
             tup = (value, bbox)
             thing.objects.append(tup)
+        # Append the initialized image class to the images container
         images.append(thing)
 
     # Generate all the image input and output data
@@ -45,21 +53,35 @@ def main():
         image.to_input()
         image.to_output()
 
+    # Print information on image n in the images container to check model accuracy
     n = 90
+    # Get the final result back from the gpu if it is stored there such that it can be displayed
     y_pred = net(images[n].x).cpu().detach().numpy()
+    # Make a matrix to store the results for the different grid windows
     pred_mat = []
+    # Print the image name such that the associated image can be easily found
     print(f"### {images[n].img_name} ###")
+    # For every grid cell get the prediction row
     for i in range(32):
+        # Get the correct elements from the output array
         row = y_pred[11*i:11*i+11]
+        # Separate the class probabilities from the other information
         row_dat = row[:5]
         row_prob = row[5:]
+        # Apply a softmax filter to the class probabilities
         row_prob = np.exp(row_prob) / sum(np.exp(row_prob))
+        # Add the two rows back together for the final matrix
         lst = list(row_dat) + list(row_prob)
-        if max(list(row_prob)) > 0.17:
+        # Print the rows which are likely to contain an object
+        if row_dat[4] > 0:
+            # Print the index of the grid cell where an object was detected and the row of probabilities
             print(f"Found object in window {i} with following probabilities: ")
-            print(list(row_prob))
+            columns = ["pillar", "black board", "white board", "plant", "forest", "qr code"]
+            df = pd.DataFrame(np.array(row_prob), columns=columns)
+            print(df)
 
         pred_mat.append(lst)
+    # Make a full 2d prediction such that this can be accessed in the debugger
     pred_mat = np.array(pred_mat)
     pass
 
